52c165fb90188ecf92fd7a6c63c52215a52897a1
HyunTaek Oh <oht222@naver.com>
2024-12-09 00:59:51 +0900

    deps: OpenAPI 문서 관련 어노테이션 적용

--- src/main/java/bbangbbangz/baby_monitoring_system/controller/BabyController.java
+++ src/main/java/bbangbbangz/baby_monitoring_system/controller/BabyController.java
@@ -1,16 +1,27 @@
 package bbangbbangz.baby_monitoring_system.controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/baby")
 public class BabyController {
 
+    @Operation(
+            summary = "아기 상태 조회",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     @GetMapping("/status")
     public String getBabyStatus() {
         // Replace with actual logic to fetch data from DB
         return "Baby is sleeping";
     }
 
+
+    @Operation(
+            summary = "아기 상태 저장",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     @PostMapping("/status")
     public String saveBabyStatus(@RequestBody String status) {
         // Replace with logic to save status to DB
