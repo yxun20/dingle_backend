ef571b71ed01bc6563f477ec8b90ddf020d4be44
HyunTaek Oh <oht222@naver.com>
2024-12-09 00:59:25 +0900

    setting: OpenAPI 관련 설정 추가

---
+++ src/main/java/bbangbbangz/baby_monitoring_system/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package bbangbbangz.baby_monitoring_system.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "빵빵이조 API 명세서",
+                description = "빵빵이조 API 명세서",
+                version = "v1"
+        )
+)
+@Configuration
+public class OpenApiConfig {
+
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String securitySchemeName = "bearerAuth";
+
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .components(components);
+    }
+}
