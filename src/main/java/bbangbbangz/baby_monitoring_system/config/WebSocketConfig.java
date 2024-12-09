fff600a713f3f0c6cbfcc20b9f80d20f6a486e22
HyunTaek Oh <oht222@naver.com>
2024-12-09 17:07:11 +0900

    init: 웹소켓 핸들러 기본 구조 추가

---
+++ src/main/java/bbangbbangz/baby_monitoring_system/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package bbangbbangz.baby_monitoring_system.config;
+
+import bbangbbangz.baby_monitoring_system.gateway.VideoAudioWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new VideoAudioWebSocketHandler(), "/stream")
+                .setAllowedOrigins("*");
+    }
+}
