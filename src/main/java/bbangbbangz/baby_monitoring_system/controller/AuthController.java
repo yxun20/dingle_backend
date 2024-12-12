5ea41365f9badaab9391c8833e770423109456cf
성영준 <s.yxun20@gmail.com>
2024-12-12 17:11:07 +0900

    Update AuthController.java

--- src/main/java/bbangbbangz/baby_monitoring_system/controller/AuthController.java
+++ src/main/java/bbangbbangz/baby_monitoring_system/controller/AuthController.java
@@ -1,5 +1,6 @@
 package bbangbbangz.baby_monitoring_system.controller;
 
+
 import bbangbbangz.baby_monitoring_system.dto.AuthRequest;
 import bbangbbangz.baby_monitoring_system.dto.AuthResponse;
 import bbangbbangz.baby_monitoring_system.dto.LoginRequest;
@@ -8,8 +9,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
+//로그인 및 회원가입 
+// JWT 생성 및 반환을 테스트하기 위해 REST API로 호출
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -19,24 +20,23 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
-
+    
     @PostMapping("/login")
     @Operation(
             summary = "유저 로그인",
-            description = "유저 로그인 시 JWT 토큰을 반환합니다."
+            description = "유저 로그인시 JWT 토큰을 반환합니다."
     )
     @ResponseBody
-    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest authRequest) {
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest authRequest) {
         String token = authService.login(authRequest);
         return ResponseEntity.ok(new AuthResponse(token));
     }
 
     @PostMapping("/register")
     @Operation(
-            summary = "유저 회원가입",
-            description = "회원가입 후 성공 메시지를 반환합니다."
+            summary = "유저 회원가입"
     )
-    public ResponseEntity<String> register(@Valid @RequestBody AuthRequest authRequest) {
+    public ResponseEntity<String> register(@RequestBody AuthRequest authRequest) {
         authService.register(authRequest);
         return ResponseEntity.ok("User registered successfully");
     }
