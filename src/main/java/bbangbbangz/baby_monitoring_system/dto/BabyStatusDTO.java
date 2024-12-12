9052e91b31f4b4d415946d5b46092fa92a738ae2
성영준 <s.yxun20@gmail.com>
2024-12-12 17:09:34 +0900

    Update BabyStatusDTO.java

---
+++ src/main/java/bbangbbangz/baby_monitoring_system/dto/BabyStatusDTO.java
@@ -0,0 +1,13 @@
+package bbangbbangz.baby_monitoring_system.dto;
+
+public class BabyStatusDTO {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
