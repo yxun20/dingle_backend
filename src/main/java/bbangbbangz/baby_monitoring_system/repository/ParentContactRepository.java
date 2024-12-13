b3f48d0089a4fc3cea80a05199c6e4ce38b081d4
성영준 <s.yxun20@gmail.com>
2024-12-13 15:28:15 +0900

    Update ParentContactRepository.java

--- src/main/java/bbangbbangz/baby_monitoring_system/repository/ParentContactRepository.java
+++ src/main/java/bbangbbangz/baby_monitoring_system/repository/ParentContactRepository.java
@@ -3,5 +3,8 @@ package bbangbbangz.baby_monitoring_system.repository;
 import bbangbbangz.baby_monitoring_system.domain.ParentContact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParentContactRepository extends JpaRepository<ParentContact, Long> {
+    Optional<ParentContact> findByUserId(Long userId);
 }
