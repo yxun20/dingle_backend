8d2d53ae53c16aeb55d6d32753f14204aefae79d
성영준 <s.yxun20@gmail.com>
2024-12-13 15:18:54 +0900

    Create ParentContactRepository.java

---
+++ src/main/java/bbangbbangz/baby_monitoring_system/repository/ParentContactRepository.java
@@ -0,0 +1,7 @@
+package bbangbbangz.baby_monitoring_system.repository;
+
+import bbangbbangz.baby_monitoring_system.domain.ParentContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentContactRepository extends JpaRepository<ParentContact, Long> {
+}
